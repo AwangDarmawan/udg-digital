8eafa671ad99c99905fe05285732950287d3c6d6
AwangDarmawan <adarmawan468@gmail.com>
2026-06-30 21:32:21 +0700

    beres

--- src/components/Admin/Preview.jsx
+++ src/components/Admin/Preview.jsx
@@ -4,7 +4,7 @@ import Sharebutton from "./ShareButton";
 const Preview = ({ submittedName, invitationLink, shareText }) => (
   <div className="max-w-3xl w-full mt-6 p-6 bg-gray-50 rounded-xl shadow-md">
     <p className="whitespace-pre-line leading-relaxed text-[#37558D] font-Montserrat">
-      Kepada Yth. <strong>{submittedName}</strong>
+      Tamu Undangan <strong>{submittedName}</strong>
 
       {"\n\n"}
 
--- src/components/Admin/Share.jsx
+++ src/components/Admin/Share.jsx
@@ -17,7 +17,8 @@ export const generateShareText = (name, invitationLink) =>
       `Bapak/Ibu/Saudara/i *${name}*\n\n` +
       `Assalamualaikum Warahmatullahi Wabarakatuh\n\n` +
       `Tanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i, teman sekaligus sahabat, untuk menghadiri acara pernikahan kami.\n\n` +
-      `Berikut link undangan kami, untuk info lengkap dari acara, bisa kunjungi: ${invitationLink}\n\n` +
+      `Berikut link undangan kami, untuk info lengkap dari acara, bisa kunjungi: 
+      ${invitationLink}\n\n` +
       `Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.\n\n` +
       `Acara akan diselenggarakan pada:\n` +
       `🗓️ Hari/Tanggal : Minggu, 05 Juli 2026\n` +
